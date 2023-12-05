d6139968948da9745d9a6df0a3286f0092349110
Kiri <kiri48787@gmail.com>
2023-12-05 17:00:37 +0800

    [feat] Completed floor plan display

---
+++ hw03/src/js/floorSVG.js
@@ -0,0 +1,9 @@
+function changeFloorSVG(checkbox) {
+    var floorNumber = checkbox.id.split('-')[1];
+    var image = document.getElementById('img-' + floorNumber);
+    if (checkbox.checked == true){
+      image.src = "../img/" + floorNumber + "_detailed.svg";
+    } else {
+      image.src = "../img/" + floorNumber + ".svg";
+    }
+  }
